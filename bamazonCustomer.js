1d1c11377cf32206540944750c3f8b18563cb28c
Jonathan Mai <h.jmai2791@gmail.com>
2018-07-15 20:40:46 -0700

    Bamazon is finished

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,6 +18,7 @@ var displayItems = function() {
     connection.query(
         "SELECT * FROM products",
         function(err, res){
+            if (err) throw err;
             for(var i = 0; i < res.length; i++){
                 console.log(res[i].id + ") " + res[i].product_name + 
                 "\nPrice: $" + res[i].price + 
@@ -25,29 +26,59 @@ var displayItems = function() {
                 "\nDepartment: " + res[i].department_name + 
                 "\n----------------------------\n");
             }
+            promptId(res);
         }
     )
 }
 
-var promptQuestion = function() {
+var promptId = function(inventory) {
     inquirer.prompt([
         {
             message: 'What is the id of the item you would like to buy?',
             type: 'input',
-            name: 'itemId'
+            name: 'itemId',
+            validate: function(ans) {
+                return !isNaN(ans);
+            }
         },
-        {
-            message: 'How many would you like to buy?',
-            type: 'input',
-            name: 'quantity'
-        }
     ]).then(ans => {
-        console.log(ans.quantity);
+        promptQuantity(ans.itemId, inventory);
     })
+}
+
+var promptQuantity = function(itemId, inventory) {
+    inquirer.prompt([
+        {
+            message: "How many do you wish to buy?",
+            type: "input",
+            name: "quantity",
+            validate: function(ans) {
+                return !isNaN(ans);
+            }
+        },
+    ]).then(ans => {
+        if(ans.quantity > inventory[itemId-1].stock_quantity){
+            console.log("We do not have enough in stock!");
+            promptQuantity(itemId, inventory);
+        }
+        else{
+            updateTable(ans.quantity, inventory[itemId-1]);
+        }
+    })
+}
+
+var updateTable = function(amount, inventory) {
+    connection.query(
+        "UPDATE products SET stock_quantity = stock_quantity - ? WHERE id = ?",
+        [amount, inventory.id],
+        function(err, res) {
+          // Let the user know the purchase was successful, re-run loadProducts
+          console.log("\nSuccessfully purchased " + amount + " " + inventory.product_name + "'s!");
+          displayItems();
+        });
 }
 
 connection.connect(function(err){
     if(err) throw err;
     displayItems();
-    promptQuestion();
 })
